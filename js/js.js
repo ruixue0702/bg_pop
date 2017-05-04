5ecf057d3f977744adff22261159e5803dd41134
ruixue0702 <ruixue0702@163.com>
2017-05-04 21:42:34 +0800

    添加js文件夹

---
+++ js/js.js
@@ -0,0 +1,11 @@
+/*
+	https://github.com/ruixue0702/bg_pop.git
+*/
+$(document).ready(function() {
+	$('body').easyBackground({
+	    wrapNeighbours: true,
+	    baseColor:"#ffffff",
+	    colors:["#FFFF00","255, 99, 71","100, 100, 100"],
+	    numParticles:30,
+	});
+});
